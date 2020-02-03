3e6ee538979cd1461ba403dc9710ad171fc29b75
geoartical <geoartical@gmail.com>
2020-02-03 16:57:39 -0600

    Primer commit

---
+++ src/app/login/register.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: [
+    './login.component.scss'
+  ]
+})
+export class RegisterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
