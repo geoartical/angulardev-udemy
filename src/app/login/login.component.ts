1cc8f8045c18713029ff179485dd9abaa603786a
geoartical <geoartical@gmail.com>
2020-02-06 09:17:21 -0600

    fin de la seccion 7

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
+declare function init_plugins();
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -9,9 +11,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public router: Router
+  ) { }
 
   ngOnInit() {
+    init_plugins();
+  }
+
+  ingresar() {
+    console.log('ingresando');
+    this.router.navigate(['/dashboard']);
   }
 
 }
